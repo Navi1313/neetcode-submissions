e75e4fb143cfec81ff2fea50cf945f14342ced8d
Navi <121182901+Navi1313@users.noreply.github.com>
2026-06-07 15:15:28 +0530

    Add: search-2d-matrix - submission-3

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-3.py
@@ -0,0 +1,40 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # brute Force : O(M*N)
+        # n = len(matrix)
+        # for i in range(0 ,n):
+        #     for j in range(0,len(matrix[0])):
+        #         if matrix[i][j] == target :
+        #             return True
+        # return False  
+
+        #  2nd Approach : O(M*LOGN)
+        # n = len(matrix)
+        # m = len(matrix[0])
+        # for i in range(n):
+        #     start = 0 
+        #     end = m-1
+        #     while start <= end:
+        #         mid = (start + end )//2
+        #         if target == matrix[i][mid]:
+        #             return True
+        #         elif target < matrix[i][mid]:
+        #             end = mid -1
+        #         else:
+        #             start = mid + 1 
+        # return False   
+
+        m = len(matrix)
+        n = len(matrix[0])
+        start , end  = 0   , m*n 
+        while start <= end : 
+            mid = (start + end) //2 
+            row_index = mid//n
+            col_index = mid%n
+            if matrix[row_index][col_index] == target :
+                return True
+            elif matrix[row_index][col_index] > target : 
+                end = mid-1
+            else:
+                start = mid+1 
+        return False                            
